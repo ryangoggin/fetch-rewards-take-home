16f902f968f0db92b115f78dfe25482f8eb0f03d
Ryan Goggin <ryangoggin1@gmail.com>
2023-10-05 15:54:35 -0400

    set up Flask API, starting post route

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+receipts = {}
+
 @app.route('/')
 def index():
     return '<h1>Fetch Rewards Take Home Assessment</h1>'
+
+def id_generator():
+    '''
+    returns a random id in xxxxxxxx-xxxx-xxxx-xxxxxxxxxxxx format
+    '''
+    return
+
+@app.route('/receipts/process', methods=['POST'])
+def process_receipts():
+    '''
+    Takes in a JSON receipt and returns a JSON object with a generated ID
+    '''
+    return
